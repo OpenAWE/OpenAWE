5dbc60a7b0c6c9a5cb83e16a8542ca29bda1ed36
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-07-09 19:32:41 +0200

    awe setup main power

--- awe.m
+++ awe.m
@@ -1,15 +1,1 @@
-awe_path = fileparts(which('awe'));
-addpath(awe_path);
-
-if exist(fullfile(awe_path, 'lib'), 'dir')
-  disp('OpenOCL is already setup in the ./lib folder. If it does not work delete the lib folder and try again.')
-else
-  websave(fullfile(awe_path, 'openocl.zip'), 'https://github.com/OpenOCL/OpenOCL/archive/a997739267e80bbc028adaee0fbce49cfef3b342.zip');
-  unzip(fullfile(awe_path, 'openocl.zip'), fullfile(awe_path, 'lib'));
-end
-
-addpath(genpath(fullfile(awe_path, 'lib')))
-
-if exist(fullfile(awe_path, 'openocl.zip'), 'file')
-  delete(fullfile(awe_path, 'openocl.zip'));
-end
+awe.setup
